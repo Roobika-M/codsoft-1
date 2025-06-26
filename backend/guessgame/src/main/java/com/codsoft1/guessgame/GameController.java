3216eeb30970a33a3f2d4dae97f98566ca7806a5
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-06-26 18:46:53 +0530

    backend is working

--- backend/guessgame/src/main/java/com/codsoft1/guessgame/GameController.java
+++ backend/guessgame/src/main/java/com/codsoft1/guessgame/GameController.java
@@ -1,50 +1,52 @@
 package com.codsoft1.guessgame;
+
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins="*")
-
+@CrossOrigin(origins = "*") 
 public class GameController {
 
-    private int target = -1;
-    private int maxguesses = 0;
+    private int targetNumber = -1;
+    private int maxGuesses = 0;
     private int guessesMade = 0;
 
-    @PostMapping("/start")
-    public Map<String,Object> startGame(@RequestParam int range){
-        target = (int)(Math.random() * range);
-        maxguesses = (int) (Math.log(range) / Math.log(2));
+    @GetMapping("/start")
+    public Map<String, Object> startGame(@RequestParam int range) {
+        targetNumber = new Random().nextInt(range + 1);
+        maxGuesses = (int) (Math.log(range) / Math.log(2));
         guessesMade = 0;
 
-        Map<String, Object>response = new HashMap<>();
-        response.put("message","Game started");
-        response.put("Maximum guesses",maxguesses);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Game started!");
+        response.put("maxGuesses", maxGuesses);
         return response;
     }
-    
+
     @PostMapping("/guess")
-    public Map<String,Object> makeGuess(@RequestParam int number){
-        Map<String, Object>response = new HashMap<>();
-        if (guessesMade >= maxguesses){
-            response.put("result", "Game over!No guesses left");
-            response.put("The number was ",target);
+    public Map<String, Object> makeGuess(@RequestParam int number) {
+        Map<String, Object> response = new HashMap<>();
+
+        if (guessesMade >= maxGuesses) {
+            response.put("result", "Game over! No guesses left.");
+            response.put("answer", targetNumber);
             return response;
         }
+
         guessesMade++;
-        if(number > target){
-            response.put("result","Too high yo");
-        }
-        else if(number < target){
-            response.put("restult","Too low bro");
-        }
-        else{
-            response.put("result","Yooooooo, you guessed it!");
-            response.put("Attempts used",guessesMade);
+
+        if (number > targetNumber) {
+            response.put("result", "Too high");
+        } else if (number < targetNumber) {
+            response.put("result", "Too low");
+        } else {
+            response.put("result", "Correct! You guessed it!");
+            response.put("attemptsUsed", guessesMade);
             return response;
         }
-        response.put("Guesses left",maxguesses - guessesMade);
+
+        response.put("guessesLeft", maxGuesses - guessesMade);
         return response;
     }
 }
